3bf492b83ce36a9a175ce63d5ff6cf96dfb2f156
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-07-01 14:32:12 +0300

    "initial commit"

---
+++ src/main/java/com/zaroyan/springsecurityjwtex2/SpringSecurityJwtEx2Application.java
@@ -0,0 +1,13 @@
+package com.zaroyan.springsecurityjwtex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtEx2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtEx2Application.class, args);
+    }
+
+}
